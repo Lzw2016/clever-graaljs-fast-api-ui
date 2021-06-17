4902f499baa7c3cefdfcd41d927ac5206023085a
lizw <1183409807@qq.com>
2021-06-17 22:02:13 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,8 @@ const COLOR_TABLE: { [name: string]: string } = {
 }
 
 loader.init().then(monaco => {
+
+  // 设置主题
   const rules: Array<monaco.editor.ITokenThemeRule> = [];
   ideaDraculaTheme.tokenColors.forEach(tokenColor => {
     if (tokenColor?.settings?.fontStyle && !tokenColor?.settings?.fontStyle?.startsWith("#")) {
@@ -62,13 +64,90 @@ loader.init().then(monaco => {
     ],
     colors: {
       ...ideaDraculaTheme.colors,
+      "editor.background": "#2B2B2B",
+      "editor.lineHighlightBackground": "#323232",
+      "editorLineNumber.foreground": "#606366",
+      "editorGutter.background": "#313335",
+      "editor.selectionHighlightBackground": "#214283",
     },
     encodedTokensColors: [],
   });
+
+  // 设置语法检测
+  monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: false,
+    noSyntaxValidation: false,
+    noSuggestionDiagnostics: false,
+  });
+  // 编译设置
+  monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+    ...monaco.languages.typescript.javascriptDefaults.getCompilerOptions(),
+    target: monaco.languages.typescript.ScriptTarget.ES2020,
+    allowNonTsExtensions: true,
+    noLib: true,
+  });
+
+  // extra libraries
+  const libSource = `
+declare class Facts {
+  /**
+   * Returns the next fact
+   */
+  static next(): string
+}
+  `;
+  const libUri = 'facts.d.ts';
+  monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
+  // When resolving definitions and references, the editor will try to use created models.
+  // Creating a model for the library allows "peek definition/references" commands to work with the library.
+  monaco.editor.createModel(libSource, 'typescript', monaco.Uri.parse(libUri));
 });
 
 function App() {
-  const [value, setValue] = useState("// some comment")
+  const [value, setValue] = useState(`
+// some comment
+\n\n\n\n\n\n
+const log = LoggerFactory.getLogger("logger");
+const jdbc = JdbcDatabase.getDefault();
+
+const sql = "select baseprice from  tb_gos_sale_saleorderdet where baseprice>0 limit 1";
+log.info("String        --> {}", jdbc.queryString(sql));
+log.info("Double        --> {}", jdbc.queryDouble(sql));
+log.info("BigDecimal    --> {}", jdbc.queryBigDecimal(sql));
+
+const sql2 = "select issettlement from  tb_gos_sale_saleorderdet where issettlement is not null limit 1";
+log.info("Boolean   --> {}", jdbc.queryBoolean(sql2));
+
+const sql3 = "select createtime from  tb_gos_sale_saleorderdet where issettlement is not null limit 1";
+log.info("Date   --> {}", jdbc.queryDate(sql3));
+
+const sql4 = "select * from  tb_merchandise_ext where update_at>:startDate";
+log.info("Count  --> {}", jdbc.queryCount(sql4, {startDate: DateUtils.parseDate("2021-01-27 15:38:31")}));
+
+const sql5 = "select prod_no,branch_id,merchandise_type,merchandise_state from tb_merchandise_ext limit 3";
+log.info("queryList --> {}", [jdbc.queryList(sql5)]);
+
+const sql6 = "select prod_no,branch_id,merchandise_type,merchandise_state from tb_merchandise_ext where prod_no=:prodNo";
+log.info("queryList --> {}", [jdbc.queryList(sql6, {prodNo: "A000212131002"})]);
+log.info("queryMap  --> {}", jdbc.queryMap(sql6, {prodNo: "A000212131002"}));
+
+const sql7 = "select prod_no,branch_id,merchandise_type,merchandise_state from tb_merchandise_ext where prod_no='A000212131002'";
+log.info("queryMap  --> {}", jdbc.queryMap(sql7));
+
+const res1 = jdbc.queryTableMap("tb_merchandise_ext", {prod_no: 'A000212131002'}, false);
+const res2 = jdbc.queryTableMap("tb_merchandise_ext", {prodNo: 'A000212131002'}, true);
+log.info("res1  -> {}", res1);
+log.info("res2  -> {}", res2);
+
+const res3 = jdbc.queryTableList("tb_merchandise_ext", {purchaseMobile: '13006155525'}, true);
+log.info("res3  -> {}", res3.size());
+
+const sql8 = "select prod_no,branch_id,merchandise_type,merchandise_state from  tb_merchandise_ext where update_at>:startDate"
+log.info("queryByPage   -> {}", jdbc.queryByPage(sql8, {pageNo: 5, pageSize: 3}, {startDate: DateUtils.parseDate("2021-01-27 15:38:31")}));
+\n\n\n\n\n\n
+return Facts.next();
+\n\n\n\n\n\n
+  `)
 
   function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor, monaco: Monaco) {
     // Alt + / --> 智能提示
@@ -91,14 +170,22 @@ function App() {
 
   return (
     <Editor
-      height="100%"
+      height="90%"
       defaultLanguage="javascript"
       defaultValue={value}
-      theme={"idea-dracula-theme"}
+      theme={"vs-dark"}
       options={{
         minimap: { enabled: false },
+        scrollbar: {
+          vertical: "visible",
+          horizontal: "visible",
+          verticalScrollbarSize: 8,
+          horizontalScrollbarSize: 8,
+          arrowSize: 16
+        }
       }}
       onMount={handleEditorDidMount}
+
     />
   )
 }
