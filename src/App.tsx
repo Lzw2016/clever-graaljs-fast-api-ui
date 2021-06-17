f2ef38ff8c940b62abff6167f718af3b1f067382
lizw <1183409807@qq.com>
2021-06-17 19:14:00 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,106 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import Editor, { loader, Monaco } from "@monaco-editor/react";
+import ideaDraculaTheme from '@/assets/idea-dracula-theme.json'
+import { TypeEnum, variableTypeOf } from '@/utils/typeof'
+import './assets/App.css'
+
+loader.config({
+  "vs/nls": {
+    availableLanguages: {
+      '*': 'zh-cn'
+    }
+  },
+});
+
+const COLOR_TABLE: { [name: string]: string } = {
+  black: "#000000",
+  silver: "#C0C0C0",
+  gray: "#808080",
+  white: "#FFFFFF",
+  maroon: "#800000",
+  red: "#FF0000",
+  purple: "#800080",
+  fuchsia: "#FF00FF",
+  green: "#008000",
+  lime: "#00FF00",
+  olive: "#808000",
+  yellow: "#FFFF00",
+  navy: "#000080",
+  blue: "#0000FF",
+  teal: "#008080",
+  aqua: "#00FFFF",
+}
+
+loader.init().then(monaco => {
+  const rules: Array<monaco.editor.ITokenThemeRule> = [];
+  ideaDraculaTheme.tokenColors.forEach(tokenColor => {
+    if (tokenColor?.settings?.fontStyle && !tokenColor?.settings?.fontStyle?.startsWith("#")) {
+      if (COLOR_TABLE[tokenColor.settings.fontStyle]) {
+        tokenColor.settings.fontStyle = COLOR_TABLE[tokenColor.settings.fontStyle];
+      }
+    }
+    if (tokenColor?.settings?.foreground && !tokenColor?.settings?.foreground?.startsWith("#")) {
+      if (COLOR_TABLE[tokenColor.settings.foreground]) {
+        tokenColor.settings.foreground = COLOR_TABLE[tokenColor.settings.foreground];
+      }
+    }
+    if (variableTypeOf(tokenColor.scope) === TypeEnum.string) {
+      rules.push({ token: tokenColor.scope as string, ...tokenColor.settings });
+    } else if (variableTypeOf(tokenColor.scope) === TypeEnum.array) {
+      (tokenColor.scope as string[]).forEach(scope => {
+        rules.push({ token: scope, ...tokenColor.settings });
+      });
+    }
+  });
+
+  monaco.editor.defineTheme("idea-dracula-theme", {
+    base: "vs-dark",
+    inherit: true,
+    rules: [
+      ...rules
+    ],
+    colors: {
+      ...ideaDraculaTheme.colors,
+    },
+    encodedTokensColors: [],
+  });
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("// some comment")
+  
+  function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor, monaco: Monaco) {
+    // Alt + / --> 智能提示
+    editor.addCommand(
+      monaco.KeyMod.Alt | monaco.KeyCode.US_SLASH,
+      () => editor.trigger(null, 'editor.action.triggerSuggest', {}),
+      '!findWidgetVisible && !inReferenceSearchEditor && !editorHasSelection'
+    );
+    // Ctrl + Shift + U --> 选中内容转大写
+    editor.addCommand(
+      monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KEY_U,
+      () => editor.trigger(null, 'editor.action.transformToUppercase', {}),
+    );
+    // Ctrl + Shift + I --> 选中内容转小写
+    editor.addCommand(
+      monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KEY_I,
+      () => editor.trigger(null, 'editor.action.transformToLowercase', {}),
+    );
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Editor
+        height="60vh"
+        defaultLanguage="javascript"
+        defaultValue={value}
+        theme={"idea-dracula-theme"}
+        options={{
+          minimap: { enabled: false },
+        }}
+        onMount={handleEditorDidMount}
+      />
     </div>
   )
 }
