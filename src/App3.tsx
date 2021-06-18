22dd0909fa4bd8240e5f4dbedbb1c225d9ebf698
lizw <1183409807@qq.com>
2021-06-18 16:49:08 +0800

    init

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-// @ts-ignore
-import Alert, { Container } from '@jetbrains/generator-ring-ui/react';
 
 interface AppProps {
 
@@ -12,14 +10,7 @@ interface AppState {
 class App extends React.Component<AppProps, AppState> {
   render() {
     return (
-      <Container>
-        <Alert
-          key={"001"}
-          type={Alert.Type.ERROR}
-        >
-          系统错误123
-        </Alert>
-      </Container>
+      123
     );
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import { ConfigProvider } from "antd";
+import antdZhCN from "antd/lib/locale/zh_CN";
+import { ConfigProviderProps } from "antd/es/config-provider";
 import './assets/index.css'
 import App from './App3'
 
+dayjs.locale("zh-cn");
+const antdConfig: ConfigProviderProps = {
+  componentSize: "small",
+  locale: antdZhCN,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ConfigProvider {...antdConfig}>
+      <App/>
+    </ConfigProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 )
