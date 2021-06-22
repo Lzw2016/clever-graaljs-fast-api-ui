1bfd2c6b8de0639b40521fc43ba6fa746bf1b653
lizw <1183409807@qq.com>
2021-06-22 18:20:58 +0800

    init

--- src/pages/App2.tsx
+++ src/pages/App2.tsx
@@ -21,21 +21,40 @@ class ReflexAdvancedDemo extends React.Component<ReflexAdvancedDemoProps, Reflex
     console.log("state -> ", JSON.stringify(this.state))
     const { size, show } = this.state;
     return (
-      <ReflexContainer orientation="horizontal">
-        <ReflexElement style={{ backgroundColor: "#222" }} direction={[-1, 1]}>
-          <button onClick={() => this.setState({ show: true })}>折叠</button>
-        </ReflexElement>
-        <ReflexSplitter style={{ height: 30, display: show ? "unset" : "none", backgroundColor: "#777" }}>
-          <button onClick={() => this.setState({ show: !show })}>折叠</button>
-        </ReflexSplitter>
-        <ReflexElement
-          style={{ backgroundColor: "#bbb" }}
-          size={show ? size : 0}
-          direction={-1}
-          onStopResize={e => this.setState({ size: (e.domElement as any)?.offsetHeight })}
-        >
-        </ReflexElement>
-      </ReflexContainer>
+      <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+        <ReflexContainer orientation="horizontal">
+          <ReflexElement style={{ backgroundColor: "#222" }} direction={[-1, 1]}>
+            <ReflexContainer orientation="vertical">
+              <ReflexElement style={{ backgroundColor: "#333" }}>
+                <button onClick={() => this.setState({ show: true })}>折叠</button>
+              </ReflexElement>
+              <ReflexSplitter propagate={false}/>
+              <ReflexElement style={{ backgroundColor: "#444" }}>
+                222
+              </ReflexElement>
+              <ReflexSplitter propagate={false}/>
+              <ReflexElement style={{ backgroundColor: "#555" }}>
+                333
+              </ReflexElement>
+            </ReflexContainer>
+          </ReflexElement>
+          <ReflexSplitter propagate={false} style={{ height: 30, display: show ? "unset" : "none", backgroundColor: "#777" }}>
+            <button onClick={() => this.setState({ show: !show })}>折叠</button>
+          </ReflexSplitter>
+          <ReflexElement
+            style={{ backgroundColor: "#bbb" }}
+            size={show ? size : 0}
+            // minSize={show ? 64 : 0}
+            direction={-1}
+            onStopResize={e => this.setState({ size: (e.domElement as any)?.offsetHeight })}
+          >
+          </ReflexElement>
+        </ReflexContainer>
+
+        <div style={{ height: 64, flexShrink: 0, flexGrow: 0, display: "flex", flexDirection: "row" }}>
+
+        </div>
+      </div>
     )
   }
 }
