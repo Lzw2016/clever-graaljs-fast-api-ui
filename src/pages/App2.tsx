3489cf2b57ae057456ab025023ecbacb6425d0d1
lizw <1183409807@qq.com>
2021-06-22 16:58:48 +0800

    init

--- src/pages/App2.tsx
+++ src/pages/App2.tsx
@@ -6,129 +6,34 @@ interface ReflexAdvancedDemoProps {
 }
 
 interface ReflexAdvancedDemoState {
+  size: number;
+  show: boolean;
 }
 
 class ReflexAdvancedDemo extends React.Component<ReflexAdvancedDemoProps, ReflexAdvancedDemoState> {
 
   constructor(props: Readonly<ReflexAdvancedDemoProps>) {
     super(props);
-  }
-
-  resizeProps = {
-    onStopResize: this.onStopResize.bind(this),
-    onResize: this.onResize.bind(this)
-  }
-
-  onResize(e: any) {
-    if (e.domElement) {
-      e.domElement.classList.add('resizing')
-    }
-  }
-
-  onStopResize(e: any) {
-    if (e.domElement) {
-      e.domElement.classList.remove('resizing')
-    }
+    this.state = { size: 200, show: true };
   }
 
   render() {
+    console.log("state -> ", JSON.stringify(this.state))
+    const { size, show } = this.state;
     return (
       <ReflexContainer orientation="horizontal">
-        <ReflexElement className="header" flex={0.1}>
-          <div className="pane-content">
-            <label>
-              Header (fixed)
-            </label>
-          </div>
+        <ReflexElement style={{ backgroundColor: "#222" }} direction={[-1, 1]}>
+          <button onClick={() => this.setState({ show: true })}>折叠</button>
         </ReflexElement>
-        <ReflexElement>
-          <ReflexContainer orientation="vertical">
-            <ReflexElement {...this.resizeProps}>
-              <ReflexContainer orientation="horizontal">
-                <ReflexElement {...this.resizeProps}>
-                  <div className="pane-content">
-                    <div style={{ height: '30%' }}/>
-                    <label style={{ height: '0%' }}>
-                      Left Pane <br/> Top
-                      <br/>
-                      (splitter propagation)
-                    </label>
-                  </div>
-                </ReflexElement>
-                <ReflexSplitter propagate={true} {...this.resizeProps}/>
-                <ReflexElement {...this.resizeProps}>
-                  <div className="pane-content">
-                    <div style={{ height: '30%' }}/>
-                    <label style={{ height: '0%' }}>
-                      Left Pane <br/> Middle
-                      <br/>
-                      (splitter propagation)
-                    </label>
-                  </div>
-                </ReflexElement>
-                <ReflexSplitter propagate={true} {...this.resizeProps}/>
-                <ReflexElement {...this.resizeProps}>
-                  <div className="pane-content">
-                    <div style={{ height: '30%' }}/>
-                    <label style={{ height: '0%' }}>
-                      Left Pane <br/> Bottom
-                      <br/>
-                      (splitter propagation)
-                    </label>
-                  </div>
-                </ReflexElement>
-              </ReflexContainer>
-            </ReflexElement>
-            <ReflexSplitter {...this.resizeProps}/>
-            <ReflexElement flex={0.5} {...this.resizeProps}>
-              <div className="pane-content">
-                <label>
-                  Middle Pane
-                </label>
-              </div>
-            </ReflexElement>
-            <ReflexSplitter{...this.resizeProps}/>
-            <ReflexElement {...this.resizeProps}>
-              <ReflexContainer orientation="horizontal">
-                <ReflexElement {...this.resizeProps}>
-                  <div>
-                    <ReflexContainer orientation="vertical">
-                      <ReflexElement {...this.resizeProps}>
-                        <div className="pane-content">
-                          <label>
-                            Right Pane <br/> Upper-Left
-                          </label>
-                        </div>
-                      </ReflexElement>
-                      <ReflexSplitter/>
-                      <ReflexElement {...this.resizeProps}>
-                        <div className="pane-content">
-                          <label>
-                            Right Pane <br/> Upper-Right
-                          </label>
-                        </div>
-                      </ReflexElement>
-                    </ReflexContainer>
-                  </div>
-                </ReflexElement>
-                <ReflexSplitter {...this.resizeProps}/>
-                <ReflexElement {...this.resizeProps}>
-                  <div className="pane-content">
-                    <label>
-                      Right Pane <br/> Bottom
-                    </label>
-                  </div>
-                </ReflexElement>
-              </ReflexContainer>
-            </ReflexElement>
-          </ReflexContainer>
-        </ReflexElement>
-        <ReflexElement className="footer" flex={0.1}>
-          <div className="pane-content">
-            <label>
-              Footer (fixed)
-            </label>
-          </div>
+        <ReflexSplitter style={{ height: 30, display: show ? "unset" : "none", backgroundColor: "#777" }}>
+          <button onClick={() => this.setState({ show: !show })}>折叠</button>
+        </ReflexSplitter>
+        <ReflexElement
+          style={{ backgroundColor: "#bbb" }}
+          size={show ? size : 0}
+          direction={-1}
+          onStopResize={e => this.setState({ size: (e.domElement as any)?.offsetHeight })}
+        >
         </ReflexElement>
       </ReflexContainer>
     )
