c33aad2b9a6af60dd44f8d58dfb312fe726b14aa
lizw <1183409807@qq.com>
2021-07-12 22:43:54 +0800

    62%

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./"),
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src/"),
     },
   },
   css: {
